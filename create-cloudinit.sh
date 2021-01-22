0e33ffc0db87f0664107c421e1b938f2dddd8156
pendulm <lonependulm@gmail.com>
2021-01-22 21:58:00 +0800

    auto setup dns

--- create-cloudinit.sh
+++ create-cloudinit.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-domain=$1
+# export LIBVIRT_DEFAULT_URI=qemu:///system
+
+add_dns() {
+    local domain=$1
+    local mac=$(virsh dumpxml $domain|awk -F"'" '/mac address/{print $2}')
+    [[ -z $mac ]] && return
+    local ip=$(virsh --connect qemu:///system net-dhcp-leases default|grep ${mac}|awk '{print $5}'|awk -F/ '{print $1}')
+    virsh --connect qemu:///system  net-update default add  dns-host \
+        "<host ip='${ip}'>
+            <hostname>${domain}</hostname>
+            </host>" \
+                --live --config
+}
+
+domain=${1:-fedora33}
+tmp_cloud_conf=/tmp/tmp-${domain}.yaml
+sed "/fqdn/s/fedora33/${domain}/" cloudinit-user-data.yaml > $tmp_cloud_conf
 virt-install \
     --name $domain \
     --memory 1024 \
     --noreboot \
     --os-variant detect=on,name=fedora-unknown \
-    --cloud-init "user-data=${PWD}/cloudinit-user-data.yaml,ssh-key=${HOME}/.ssh/id_rsa.pub" \
-    --disk=size=10,backing_store="${PWD}/Fedora-Cloud-Base-33-1.2.x86_64.qcow2"
-    # -w network=default \
+    --cloud-init "user-data=${tmp_cloud_conf},ssh-key=${HOME}/.ssh/id_rsa.pub" \
+    --disk=size=5,backing_store="${PWD}/Fedora-Cloud-Base-33-1.2.x86_64.qcow2" \
+    --disk=size=6 \
+    --network bridge=virbr0
+add_dns $domain
+rm -f $tmp_cloud_conf
--- del.sh
+++ del.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+# export LIBVIRT_DEFAULT_URI=qemu:///system
+del_dns() {
+    local domain=$1
+    local mac=$(virsh dumpxml $domain|awk -F"'" '/mac address/{print $2}')
+    [[ -z $mac ]] && return
+    local ip=$(virsh --connect qemu:///system net-dhcp-leases default|grep ${mac}|awk '{print $5}'|awk -F/ '{print $1}')
+    virsh --connect qemu:///system  net-update default delete  dns-host \
+        "<host ip='${ip}'>
+            <hostname>${domain}</hostname>
+            </host>" \
+                --live --config
+}
 
-domain=$1
+domain=${1:-fedora33}
+del_dns $domain
 virsh destroy $domain
+sleep 2
 virsh undefine $domain --remove-all-storage
